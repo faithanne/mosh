6d1ead9d0ed1d9a29faa6a980eb5d7469a4774c9
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-06-11 19:02:23 -0400

    Updating Request and Software to reflect SoftwareCategory FK.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
 class Request < ActiveRecord::Base
   attr_accessible :comment, :image_id, :instructions, :name, :request_status_id, :software_category_id, :source_url, :user_id, :year
+  belongs_to :software_category
 end
--- app/models/software.rb
+++ app/models/software.rb
@@ -2,4 +2,5 @@ class Software < ActiveRecord::Base
   attr_accessible :comment, :name, :software_category_id, :source_url, :version, :year
   has_many :installs
   has_many :softwares, through: :installs
+  belongs_to :software_category
 end
