384cf7022736915afc109ded04d8422327526278
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-06-10 22:00:28 -0400

    Creating Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mosh::Application.config.secret_token = '7d5583c4f1bfd6892b1db24624ef2eaf8daaf9d495aaf7775c0ab31623af8aa3182ed7f850ea68bbbc325cf85b9e7ea0f1c7af9bca081394ccc9f53d6146bb17'
