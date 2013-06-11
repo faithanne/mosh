b4e102c8794889769dbc70387a5741f49c1c8493
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-06-10 23:36:10 -0400

    Removing install_id

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   attr_accessible :name, :solicit_start, :submit_deadline, :test_deadline
+  has_many :installs
+  has_many :softwares, through: :installs
 end
--- app/models/software.rb
+++ app/models/software.rb
@@ -1,3 +1,5 @@
-Nclass Software < ActiveRecord::Base
+class Software < ActiveRecord::Base
   attr_accessible :comment, :name, :software_category_id, :source_url, :version, :year
+  has_many :installs
+  has_many :softwares, through: :installs
 end
