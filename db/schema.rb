39418028b5568d8ab5533ee94859d5e27fc26b65
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-10-31 12:00:24 -0400

    Adding Name to Locations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130924151526) do
+ActiveRecord::Schema.define(:version => 20131031155914) do
 
   create_table "after_hours_accesses", :force => true do |t|
     t.integer  "location_id"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(:version => 20130924151526) do
     t.text     "comment"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "location"
   end
 
   create_table "rails_admin_histories", :force => true do |t|
