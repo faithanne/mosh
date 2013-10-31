0a18cb8c21658b3d23f49d023b79277dba4ac23b
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-10-31 11:52:24 -0400

    Fixing NonCSIT filter logic.

--- lib/mosh_modules/noncsit_module.rb
+++ lib/mosh_modules/noncsit_module.rb
@@ -2,7 +2,7 @@ module MoshModules
   module NoncsitModule
 
     def noncsit_filter
-      if !user_signed_in? || !current_user.non_csit
+      if !user_signed_in? || current_user.non_csit
         render file: "#{Rails.root}/public/404.html", status: 404, layout: false
       end
     end
