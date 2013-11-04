55235d979fe37c7a0edfc862d91a3f546d6b1359
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-11-04 14:36:03 -0500

    Adding name to SoftwareTests.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131104193219) do
+ActiveRecord::Schema.define(:version => 20131104193500) do
 
   create_table "after_hours_accesses", :force => true do |t|
     t.integer  "location_id"
@@ -172,6 +172,7 @@ ActiveRecord::Schema.define(:version => 20131104193219) do
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
     t.integer  "software_id"
+    t.string   "name"
   end
 
   create_table "softwares", :force => true do |t|
