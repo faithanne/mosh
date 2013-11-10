0591b0cd987a67c8010bee3643fa34c5908a275c
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-11-09 23:46:35 -0500

    Fixing Installs Association.

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,7 +1,7 @@
 class Software < ActiveRecord::Base
   attr_accessible :comment, :name, :software_category_id, :source_url, :version, :year, :software_install_id, :software_test_id
   has_many :installs
-  has_many :softwares, through: :installs
+  has_many :images, through: :installs
   belongs_to :software_category
   has_many :silent_installs
   has_many :software_tests
